ddcd3215716fddc3326276e7fa93228627aebae7
polankoak <45658273+polankoak@users.noreply.github.com>
2020-10-09 20:19:03 -0300

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,5 +3,5 @@ echo "****************"
 echo "* Building jar!*" 
 echo "****************" 
 
-PROJ=/home/tsoft/jenkins/pipeline
-docker run --rm -v /root/.m2:/root/.m2 -v $PROJ/java-app:/app -w /app maven:3-alpine "$@"
+#PROJ=/home/tsoft/jenkins/pipeline
+docker run --rm -v /root/.m2:/root/.m2 -v /home/tsoft/jenkins/pipeline/java-app:/app -w /app maven:3-alpine "$@"
